05835eb3b0ba6d743af7a6ff1ad398bf5049cf21
yakatyansh <ykpetrolhead08@outlook.com>
2025-01-25 14:01:29 +0530

    unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 "use client"
-
-import React, { useState, useEffect } from "react";
 import WhiteFlower from "../components/WhiteFlower";
 
 export default function Home() {
 
   return (
-    <div >
+    <div>
       <WhiteFlower />
     </div>
   );
